0c96843f210cd73bcc3366d04ee20cf61fc260c0
Claudio <claudio.antonius@weekendinc.com>
2021-06-25 17:02:00 +0700

    try tesseract

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { getTesseract } from './components/Tesseract';
 
 function App() {
+  const [url, setUrl] = React.useState('');
+  const [data, setData] = React.useState('');
+  const [loading, setLoading] = React.useState(false);
+
+  const handleGet = async () => {
+    setLoading(true);
+    setData('loading...');
+    const temp = await getTesseract(url);
+    if (temp) setData(temp);
+    if (temp) setLoading(false);
+  };
+
+  const handleOnChange = (e) => {
+    setUrl(e.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="App">
+      <h4>Image to Text</h4>
+      <form>
+        <label>name</label>
+        <input 
+          name="surat"
+          type="text"
+          onChange={handleOnChange}
+          disabled={loading}
+        />
+        <div>
+          <button 
+            onClick={handleGet}
+            disabled={loading}
+          >get data</button>
+        </div>
+      </form>
+      <div>
+        {data}
+      </div>
+    </section>
   );
 }
 
